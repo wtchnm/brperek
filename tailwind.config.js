76c6a9eb49097caf91a249260c12ec269f0ee91a
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2022-03-13 19:49:54 -0300

    feat: add Roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-	content: ['./src/**/*.astro']
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Roboto', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	}
 }
