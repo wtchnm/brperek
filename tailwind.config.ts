89c81291f650a525fa0f35ea59c559fc8b9fab84
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2022-04-17 00:28:35 -0300

    chore: convert tailwind config to ts

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import {
+	TailwindConfig,
+	TailwindValuesFontFamily
+} from 'tailwindcss/tailwind-config'
+
+const config: TailwindConfig = {
+	content: ['src/**/*.astro'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				body: `url('/background.webp')`
+			},
+			colors: {
+				background: '#141414'
+			},
+			textColor: {
+				primary: '#FFFFFF',
+				secondary: '#61803A',
+				tertiary: '#9D9C9C'
+			},
+			fontFamily: {
+				sans: [
+					'Roboto',
+					...(defaultTheme.fontFamily as TailwindValuesFontFamily).sans
+				]
+			},
+			letterSpacing: {
+				full: '0.2em'
+			}
+		}
+	}
+}
+export default config
