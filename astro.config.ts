d10ff0844e423502612984e6fd5d7dba6e99cbf0
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2022-11-18 11:26:26 -0300

    perf: inline @font-face

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-	integrations: [tailwind({ config: { applyBaseStyles: false } })]
+	integrations: [tailwind()]
 })
